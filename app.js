505aa8064c84b5cba0653fb00f6b6a16ef37aad3
GArquez <guillegarquez5@gmail.com>
2022-08-25 17:37:14 -0300

    Quinto commit: se agrega contenido HTML/CSS, se aplica la app de Asocimiento a la seccion homonima, en la cual se trabaja con la API Storage para verificar si existe un asociado con tal nombre.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*Simulador para asociarse, ingresa nombres, ubicacion y duracion, para devolver lo mismo con el numero de asociamiento correspondiente y el precio a pagar*/
 
 let principal = document.getElementById("Main")
-principal.className = "container d-flex flex-column align-items-center"
+principal.className = "d-flex flex-column align-items-center justify-content-between"
 
 let caja0 = document.getElementById("Socio")
 let caja1 = document.getElementById("Duracion")
@@ -10,12 +10,8 @@ let caja3 = document.getElementById("Enviar")
 caja0.className ="col-md-3 d-flex flex-column align-items-center"
 caja1.className ="col-md-3 d-flex flex-column align-items-center"
 caja2.className ="col-md-3 d-flex flex-column align-items-center"
-caja3.className ="col-md-3 d-flex flex-column align-items-center"
+caja3.className ="col-md-2 offset-md-1 d-flex flex-column align-items-center"
 
-const titular = document.getElementById("titular")
-const parrafo = document.getElementById("parrafo")
-titular.innerHTML = "Asociate al Club Atlético Tucumán"
-parrafo.innerHTML = "Mediante esta app vas a poder asociarte. Ingresa tu nombre completo, elegí entre 6 y 12 meses junto a la ubicacion deseada ¡y tenes tu pack de socio!"
 
 
 const socioPrecio = (duracion, sector) => {
@@ -40,49 +36,62 @@ miFormulario.addEventListener("submit", validarFormulario);
 function validarFormulario(e){
     e.preventDefault();
     let formulario = e.target
-    let socio = (formulario.children[0].value)
-    let duracion = (formulario.children[1].value)
-    let sector = (formulario.children[2].value)
+    let socio = (formulario.children[1].value)
+    let duracion = (formulario.children[2].value)
+    let sector = (formulario.children[3].value)
 
     socioPrecio(duracion, sector)
 
     const packSocio = [
         { Ubicacion: "Platea",
+        Identificacion : socio,
         Precio:"$" + socioPrecio(duracion,sector),
         Tiempo: duracion + " meses" }, 
         { Ubicacion: "Laprida",
+        Identificacion : socio,
         Precio:"$" + socioPrecio(duracion,sector),
         Tiempo: duracion + " meses" }, 
         { Ubicacion: "Bolivia",
+        Identificacion : socio,
         Precio:"$" + socioPrecio(duracion,sector),
         Tiempo: duracion + " meses"},
         { Ubicacion: "Chile",
+        Identificacion : socio,
         Precio:"$" + socioPrecio(duracion,sector),
         Tiempo: duracion + " meses"} ]
 
+
+        let SocioMemory = localStorage.getItem('id')
+
         
-        if (socio != "") {
+        if (SocioMemory != socio) {
             const buscador = packSocio.find((el) => el.Ubicacion == sector)
             
             while (duracion == 6 && buscador) {
             let div = document.getElementById("Constructor")
             div.className = "d-flex flex-column align-items-center"
             let caja = `<h2>¡Hola! Este es tu pack de socio</h2>
+                        <p>Nombre: ${buscador.Identificacion}</p>
                         <p>Ubicacion: ${buscador.Ubicacion}</p>
                         <p>Precio: ${buscador.Precio}</p>
                         <p>Tiempo: ${buscador.Tiempo}</p>`
+            
+            localStorage.setItem('pack', socio)            
             return div.innerHTML = caja
             }    
             while (duracion == 12 && buscador) {
             let div = document.getElementById("Constructor")
             div.className = "d-flex flex-column align-items-center"
-            let caja = `<h2>Este es tu pack de socio</h2>
+            let caja = `<h2>¡Hola! Este es tu pack de socio</h2>
+                        <p>Nombre: ${buscador.Identificacion}</p>
                         <p>Ubicacion: ${buscador.Ubicacion}</p>
                         <p>Precio: ${buscador.Precio}</p>
                         <p>Tiempo: ${buscador.Tiempo}</p>`
+            
+            localStorage.setItem('id', socio)            
             return div.innerHTML = caja
             }
         } else {
-        alert ("Por favor, Ingresar nombre y apellido completo para asociarse")
+        alert ("El socio con nombre " + socio + " ya existe en nuestro padron.")
     }
 }
